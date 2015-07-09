20930698caa64c2deb07cc847ee4852e70137cbf
Markus Bergholz <markuman@gmail.com>
2015-07-09 20:31:02 +0200

    some more improvements by nick

--- repelem.m
+++ repelem.m
@@ -41,31 +41,42 @@ function ret = repelem(element, varargin)
         error("%gD Array objects requires %g input arguments, only %g given",ndims(element),ndims(element)+1,nargin);
       endif
       
-    elseif isvector(element)&&(length(v) == length(element))
+    elseif isvector(element) && (length(v) == length(element))
     # this assumes a vector. but a 2x2 array and v = [2 2] would pass, but should error out.
     
-      if isvector(element)
-        # in any case, transform into row vector
-        Delement  = element(:)';
-        Dv        = v(:)';
-        
-        ret = arrayfun(@(Delement,Dv) repmat(Delement,1,Dv), Delement, Dv, 'UniformOutput', false);
-        ret = [ret{:}];
-        
-        if iscolumn(element)
-          # it was a column vector
-          ret = ret';
-        endif
-      
-      endif
+      # works for row or column vector. output direction will match element
+      idx1 = cumsum(v); # gets ending position for each element item
+      idx2(1:idx1(end)) = 0; # row vector with enough space for output
+      idx2(idx1(1:end-1)+1) = 1; # sets starting position of each element to 1
+      idx2(1) = 1; # sets starting position of each element to 1
+      ret = element(cumsum(idx2)); # cumsum fills array with element indices is right position. element(cumsum) fills with element values, direction matches element.
+  
       
     else
       error("varargin{1} must be a scalar or the same length as element")
+      
     endif
     
   elseif (nargin > 2)
+  
+    ## INPUT CHECK
+    elsize = ndims(element);
+    nonscalarv = ~cellfun(@isscalar,varargin);
+    
+    # first check for valid number of inputs (compatibility: ML allows for unlimited trailing singletons)
+    if (nargin-1 < elsize) %will allow for trailing singletons
+      error("%gD Array objects requires %g input arguments, only %g given",ndims(element),ndims(element)+1,nargin);
+
+    # 2nd that they are all scalars or vectors. isvector gives true for scalars.
+    elseif (~all(cellfun(@isvector,varargin))) 
+      error("varargin must be all be scalars or vectors");
+    
+    # third, that the ones that are vectors have the right length. this should catch any vectors thrown at trailing singletons, which should only have scalars.
+    elseif (~all(cellfun(@length,varargin(nonscalarv)) == size(element)(nonscalarv)))
+      error("varargin(n) must either be scalar or have the same number of elements as the size of dimension n of the array to be replicated");        
+      
+    endif 
          
-    # ret = arrayfun(@(element,N,M) repmat(element,N,M), element, N, M, 'UniformOutput', false);
     ret = arrayfun(@(element,varargin) repmat(element,varargin{:}), element, varargin{:}, 'UniformOutput', false);
     ret = cell2mat(ret);
     
